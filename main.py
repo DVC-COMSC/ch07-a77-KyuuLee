32e5a0a7fc335a1afae0f92fef66dc74bc50acc2
Kyuu <69904775+KyuuLee@users.noreply.github.com>
2022-10-01 14:31:27 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-
 num1 = list(map(int, input().split()))
 num2 = list(map(int, input().split()))
+N = len(num1) 
+M = len(num2) 
 
-# ******************************
-# Make your Code
-# ******************************
+minlen = N if N <= M else M
+num3 = [] 
+for i in range(minlen):
+	num3.append(num1[i])
+	num3.append(num2[i])
 
-# print (num3) 
+# if N == minlen:
+# 	num3 += num2[minlen:]
+# else:
+# 	num3 += num1[minlen:]
+
+num3 += num2[minlen:] if N == minlen else num1[minlen:]
+
+
+print (num3)
